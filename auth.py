ec77214c994f42cbeb5e50fdb50591fb265b6c4c
ojixzzz <ojixzzz@gmail.com>
2016-10-15 15:21:06 +0700

    auth

--- auth.py
+++ auth.py
@@ -1,11 +1,12 @@
 import os
 import json
 import googleanalytics as ga
-if os.path.exists('credentials.json'):
-    credentials = json.load(open('credentials.json'))
-else:
-    credentials = ga.authorize()
-    credentials = credentials.serialize()
-    json.dump(credentials, open('credentials.json', 'w'))
 
-ga.authenticate(**credentials, save=True)
+def credentialsX():
+    if os.path.exists('credentials.json'):
+        credentials = json.load(open('credentials.json'))
+    else:
+        credentials = ga.authorize()
+        credentials = credentials.serialize()
+        json.dump(credentials, open('credentials.json', 'w'))
+    return credentials
