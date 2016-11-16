ce63bcb21f0b824ffcbe5b481597031dd0d969a9
ojixzzz <ojixzzz@gmail.com>
2016-11-16 12:16:58 +0700

    ubah data

--- monitserver.py
+++ monitserver.py
@@ -8,10 +8,26 @@ class DataStatusResource:
 
         monit1010 = mc.get("monit1010")
         if monit1010:
-        	monit1010['api4shared_status'] = mc.get("monit1010_4shared")
-        	resp.body = json.dumps({"status": 200, "data": monit1010})
+            data = {
+                "chart1_list": monit1010['autodownload_list'],
+                "chart2_list": monit1010['link4share_list'],
+                "chart3_list": monit1010['apkpoke_list'],
+                "data1_label": "AutoDownload<br/> Online:",
+                "data2_label": "Link4share<br/> Online:",
+                "data3_label": "Apkpoke<br/> Online:",
+                "data4_label": "Api 4shared:",
+                "data5_label": "Server Status:",
+                "data6_label": "Transaksi Laundry<br/>(Today):",
+                "data1": monit1010['autodownload_online'],
+                "data2": monit1010['link4share_online'],
+                "data3": monit1010['apkpoke_online'],
+                "data4": mc.get("monit1010_4shared"),
+                "data5": monit1010['server_status'],
+                "data6": 0,
+            }
+            resp.body = json.dumps({"status": 200, "data": monit1010})
         else:
-        	resp.body = json.dumps({"status": 404})
+            resp.body = json.dumps({"status": 404})
 
 api = falcon.API()
 api.add_route('/data_status', DataStatusResource())
